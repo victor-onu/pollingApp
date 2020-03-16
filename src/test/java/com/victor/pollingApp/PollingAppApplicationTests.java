d7e85cefbdfbc89927f78144b6457dfbd4fdecda
victor-onu <victoronushaibu@gmail.com>
2020-03-16 10:28:48 +0100

    Initial commit

---
+++ src/test/java/com/victor/pollingApp/PollingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.pollingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
